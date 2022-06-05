5a952cdc9dba2eaf4d6d1986e2ad9cdb4afa70b5
tad <tad.developer@gmail.com>
2022-06-05 15:49:10 +0900

    :alembic: Each function were implemented on an experimental basis.
    * Implemented Each function
    * Refactored Get function
      * Changed implementation, use Each

--- goval.go
+++ goval.go
@@ -4,18 +4,34 @@ import "reflect"
 
 // Get get a value from a structure using a path.
 func Get(target any, path string) any {
-	p := parse(path)
-	return get(target, p)
+	var ret any
+	Each(target, path, func(v any, _ any) {
+		ret = v
+	})
+	return ret
 }
 
-func get(target any, path Path) any {
-	if len(path) == 0 {
-		return target
+// EachFunc a callback func given to each function.
+//
+// v: Value of the field specified in the path.
+// owner: Structure owning the value.
+type EachFunc func(v any, owner any)
+
+// Each executes the given function once for each field specified in the path.
+func Each(target any, path string, fn EachFunc) {
+	paths := parse(path)
+	each(nil, target, paths, fn)
+}
+
+func each(owner any, target any, relPath Path, fn EachFunc) {
+	if len(relPath) == 0 {
+		fn(target, owner)
+		return
 	}
 	v := reflect.ValueOf(target)
-	p := path[0]
+	p := relPath[0]
 
 	fv := v.FieldByName(p)
 	dist := fv.Interface()
-	return get(dist, path[1:])
+	each(target, dist, relPath[1:], fn)
 }
--- goval_test.go
+++ goval_test.go
@@ -70,3 +70,60 @@ func TestGet(t *testing.T) {
 		})
 	}
 }
+
+func TestEach(t *testing.T) {
+	type args struct {
+		target any
+		path   string
+	}
+	type test struct {
+		name      string
+		args      args
+		wantVal   any
+		wantOwner any
+	}
+	// create basic test data
+	defaultTest := func(fn func(tt test) test) test {
+		tt := test{
+			args: args{
+				target: person{
+					Name: "Alice",
+					Age:  25,
+					Address: address{
+						CountryCode: 81,
+						PostalCode:  "1000001",
+					},
+				},
+			},
+		}
+		return fn(tt)
+	}
+
+	tests := []test{
+		defaultTest(func(tt test) test {
+			tt.name = "each a field value"
+			tt.args.path = "Name"
+
+			tt.wantVal = "Alice"
+			tt.wantOwner = tt.args.target
+			return tt
+		}),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var gotVal any
+			var gotOwner any
+			godot.Each(tt.args.target, tt.args.path, func(v any, owner any) {
+				gotVal = v
+				gotOwner = owner
+			})
+
+			if !reflect.DeepEqual(gotVal, tt.wantVal) {
+				t.Errorf("Each(%v) path's value = %v, want %v", tt.args.path, gotVal, tt.wantVal)
+			}
+			if !reflect.DeepEqual(gotOwner, tt.wantOwner) {
+				t.Errorf("Each(%v) owner of value = %v, want %v", tt.args.path, gotOwner, tt.wantOwner)
+			}
+		})
+	}
+}
